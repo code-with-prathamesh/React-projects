7a695cd51cbef63edc9d5de081050f2244bb7d34
code-with-prathamesh <prathameshrajeshparte@gmail.com>
2025-07-22 10:56:11 +0530

    Creating a component.

--- basic-react-project/src/App.jsx
+++ basic-react-project/src/App.jsx
@@ -1,7 +1,21 @@
 // Each JSX expression must have one parent element, which means if you try to return multiple elements, React will throw an error.
 
 //Every JSX tag needs to be closed. You can use self-closing tags for elements that don't have children, e.g., <img src="url" />
+
 const App = () => {
+  return (
+    <div>
+      <NetflixSeries />
+      <NetflixSeries />
+      <NetflixSeries />
+      <NetflixSeries />
+      <NetflixSeries />
+      <NetflixSeries />
+    </div>
+  );
+};
+
+const NetflixSeries = () => {
   return (
     <div>
       <div>
