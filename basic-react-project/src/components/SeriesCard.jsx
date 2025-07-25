5dd61c6ab5f54a2032489912d3030d0c7f073271
code-with-prathamesh <prathameshrajeshparte@gmail.com>
2025-07-25 18:58:21 +0530

    Conditional Styling

--- basic-react-project/src/components/SeriesCard.jsx
+++ basic-react-project/src/components/SeriesCard.jsx
@@ -2,14 +2,17 @@
 
 export const SeriesCard = ({ data }) => {
   const { name, cast, genre, description, img_url, watch_url, rating } = data;
+  const ratingClass = rating >= 8.5 ? "super_hit" : "average";
 
   //Adding In-Line CSS
   const button_style = {
     padding: "1.2rem 2.6rem",
     border: "none",
     fontSize: "1.6rem",
-    backgroundColor: "var(--btn-hover-bg-color)",
-    color: "var(--bg-color)",
+    backgroundColor: `${rating >= 8.5 ? "#7dcea0" : "#f7dc6f"}`,
+    color: "var(--font-color)",
+    fontWeight: "bold",
+    cursor: "pointer",
   };
 
   return (
@@ -19,13 +22,16 @@ export const SeriesCard = ({ data }) => {
       </div>
       <div className="card-content">
         <h2>Name: {name}</h2>
-        <h3>Ratings: {rating}</h3>
+        <h3>
+          Ratings: <span className={` rating ${ratingClass}`}>{rating}</span>
+        </h3>
         <p>Summary: {description}</p>
         <p>Genre: {genre}</p>
         <p>Cast: {cast}</p>
         <a href={watch_url} target="_blank">
-          <button style={button_style}>Watch Now</button>
+          <button style={button_style}>Watch Now</button>{" "}
         </a>
+        <button>Heading</button>
       </div>
     </li>
   );
